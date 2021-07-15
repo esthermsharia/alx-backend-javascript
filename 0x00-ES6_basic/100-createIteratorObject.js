a6ecf8c60376633b082b68dcae4147ebdc955773
kimengu-david <davidmutune200@gmail.com>
2021-07-15 23:31:05 +0000

    Add advanced task 1

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,11 @@
-/* eslint-disable */
 export default function createIteratorObject(report) {
-    return Object.keys(employeesList);
+  const array1 = [];
+  /* eslint-disable */
+  for (const [department, employees] of Object.entries(report.allEmployees)) {
+    for (const emp of employees) {
+      array1.push(emp);
+    }
+  }
+
+  return array1;
 }
