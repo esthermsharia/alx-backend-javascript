5ca0c65f5285e35612f3a43ec293db3119a4453f
kimengu-david <davidmutune200@gmail.com>
2021-07-19 21:09:31 +0000

    Add task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return (`${this.name} (${this.code})`);
+  }
+
+  set name(new_Name) {
+    this._name = new_Name;
+  }
+
+  set code(new_Code) {
+    this._name = new_Code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+}
