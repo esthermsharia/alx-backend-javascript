4cb851e7268248316591869de8ddc8582abd45cb
kimengu-david <davidmutune200@gmail.com>
2021-07-21 20:25:35 +0000

    Add task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,24 +1,21 @@
-export default function getListStudents ()
-{
-    
- let myarray = [
-        {
-            firstName: 'Guillaume',
-            id: 1,
-            location: 'San Francisco',
-          },
-          {
-            firstName: 'James',
-            id: 2,
-            location: 'Columbia',
-          },
-          {
-            firstName: 'Serena',
-            id: 5,
-            location: 'San Francisco',
-          },
-        ];
-    return myarray;
-
+export default function getListStudents() {
+  let myarray = [
+    {
+      firstName: "Guillaume",
+      id: 1,
+      location: "San Francisco",
+    },
+    {
+      firstName: "James",
+      id: 2,
+      location: "Columbia",
+    },
+    {
+      firstName: "Serena",
+      id: 5,
+      location: "San Francisco",
+    },
+  ];
+  return myarray;
 }
 
